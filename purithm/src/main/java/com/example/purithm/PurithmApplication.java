9a960d47e3783f7d402bd695b16295aba2adab6a
tmddus2 <tmddus2__@naver.com>
2024-07-09 01:17:00 +0900

    🎉 feat: init project

---
+++ purithm/src/main/java/com/example/purithm/PurithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.purithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurithmApplication.class, args);
+	}
+
+}
