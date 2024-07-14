a8881c0d435acfae99fafb5ae29d7bf76d59459d
tmddus2 <tmddus2__@naver.com>
2024-07-14 21:19:12 +0900

    ✨ feat: jwt filter 추가

--- purithm/src/main/java/com/example/purithm/user/config/SecurityConfig.java
+++ purithm/src/main/java/com/example/purithm/user/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.purithm.user.config;
 
+import com.example.purithm.user.filter.JWTFilter;
 import com.example.purithm.user.handler.OAuth2AuthenticationSuccessHandler;
+import com.example.purithm.user.jwt.JWTUtil;
 import com.example.purithm.user.service.OAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,18 +18,23 @@ public class SecurityConfig {
 
   private final OAuth2UserService oAuth2UserService;
   private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
-  public SecurityConfig(OAuth2UserService oAuth2UserService, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) {
+  private final JWTUtil jwtUtil;
+
+  public SecurityConfig(OAuth2UserService oAuth2UserService, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler, JWTUtil jwtUtil) {
     this.oAuth2UserService = oAuth2UserService;
     this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
+    this.jwtUtil = jwtUtil;
   }
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .csrf(csrf -> csrf.disable())
         .formLogin(formLogin -> formLogin.disable())
+        .httpBasic(httpBasic -> httpBasic.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
         .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/api").authenticated()
+            .requestMatchers("/api/**").authenticated()
             .anyRequest().permitAll()
         )
         .oauth2Login(oauth2Login -> oauth2Login
---
+++ purithm/src/main/java/com/example/purithm/user/filter/JWTFilter.java
@@ -0,0 +1,47 @@
+package com.example.purithm.user.filter;
+
+import com.example.purithm.user.entity.CustomOAuth2User;
+import com.example.purithm.user.jwt.JWTUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JWTFilter extends OncePerRequestFilter {
+
+  private final JWTUtil jwtUtil;
+
+  public  JWTFilter(JWTUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String token = request.getHeader("Authorization");
+
+    if (token == null || !token.startsWith("Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    token = token.substring(7);
+    if (jwtUtil.isExpired(token)) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    String username = jwtUtil.getUsername(token);
+
+    CustomOAuth2User oAuth2User = new CustomOAuth2User(username);
+    Authentication authToken = new UsernamePasswordAuthenticationToken(oAuth2User, null, null);
+    SecurityContextHolder.getContext().setAuthentication(authToken);
+
+    filterChain.doFilter(request, response);
+  }
+}
