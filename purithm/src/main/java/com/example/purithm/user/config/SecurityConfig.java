e2c0932f3144c305630c56b5c801527143d5995c
tmddus2 <tmddus2__@naver.com>
2024-07-14 16:02:05 +0900

    ✨ feat: Spring Security config 추가

---
+++ purithm/src/main/java/com/example/purithm/user/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.purithm.user.config;
+
+import com.example.purithm.user.handler.OAuth2AuthenticationSuccessHandler;
+import com.example.purithm.user.service.OAuth2UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  private final OAuth2UserService oAuth2UserService;
+  private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+  public SecurityConfig(OAuth2UserService oAuth2UserService, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler) {
+    this.oAuth2UserService = oAuth2UserService;
+    this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
+  }
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .formLogin(formLogin -> formLogin.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/api").authenticated()
+            .anyRequest().permitAll()
+        )
+        .oauth2Login(oauth2Login -> oauth2Login
+            .authorizationEndpoint(authorizationEndpoint -> authorizationEndpoint
+                .baseUri("/oauth2/authorization")
+            )
+            .redirectionEndpoint(redirectionEndpoint -> redirectionEndpoint
+                .baseUri("/login/oauth2/code/*")
+            )
+            .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserService))
+            .successHandler(oAuth2AuthenticationSuccessHandler)
+        );
+
+    return http.build();
+  }
+}
