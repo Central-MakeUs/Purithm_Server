eaf1bf7525b6f67ac09df02f7466571e67f30360
tmddus2 <tmddus2__@naver.com>
2024-07-14 18:39:16 +0900

    ✨ feat: username 값 담은 JWT 토큰 반환

--- purithm/src/main/java/com/example/purithm/user/handler/OAuth2AuthenticationSuccessHandler.java
+++ purithm/src/main/java/com/example/purithm/user/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,33 +1,38 @@
 package com.example.purithm.user.handler;
 
+import com.example.purithm.user.entity.CustomOAuth2User;
+import com.example.purithm.user.jwt.JWTUtil;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import org.springframework.http.MediaType;
+
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+  private final JWTUtil jwtUtil;
+
+  public OAuth2AuthenticationSuccessHandler(JWTUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
+  }
+
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-    DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+    CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
-    String id = defaultOAuth2User.getAttributes().get("id").toString();
-    String body = """
-                    {"id":"%s"}
-                    """.formatted(id);
+    String token = jwtUtil.createJwt(oAuth2User.getName(), 60*60*60L);
 
-    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+    Cookie cookie = new Cookie("Authorization", token);
+    cookie.setMaxAge(60*60*60);
+    cookie.setPath("/api");
+    cookie.setHttpOnly(true);
 
-    PrintWriter writer = response.getWriter();
-    writer.println(body);
-    writer.flush();
+    response.addCookie(cookie);
+    response.sendRedirect("/");
   }
 }
--- purithm/src/main/java/com/example/purithm/user/service/OAuth2UserService.java
+++ purithm/src/main/java/com/example/purithm/user/service/OAuth2UserService.java
@@ -1,16 +1,14 @@
 package com.example.purithm.user.service;
 
+import com.example.purithm.user.entity.CustomOAuth2User;
 import com.example.purithm.user.entity.User;
 import com.example.purithm.user.repository.UserRepository;
-import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
+
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -23,13 +21,6 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
     OAuth2User oAuth2User = super.loadUser(userRequest);
-
-    List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_USER");
-
-    String userNameAttributeName = userRequest.getClientRegistration()
-        .getProviderDetails()
-        .getUserInfoEndpoint()
-        .getUserNameAttributeName();
 
     String provider = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
     Map<String, Object> properties = oAuth2User.getAttribute("properties");
@@ -47,7 +38,8 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
 
       userRepository.save(user);
     }
-    return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), userNameAttributeName);
+
+    return new CustomOAuth2User(username);
   }
 
 }
