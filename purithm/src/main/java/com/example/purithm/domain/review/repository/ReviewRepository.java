1317d196e64c947034b155fd1f850a84511e5450
tmddus2 <tmddus2__@naver.com>
2024-08-11 23:54:23 +0900

    🩹 chore: repository 어노테이션 추가

--- purithm/src/main/java/com/example/purithm/domain/review/repository/ReviewRepository.java
+++ purithm/src/main/java/com/example/purithm/domain/review/repository/ReviewRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.purithm.domain.review.entity.Review;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 	List<Review> findAllByFilterId(Long filterId);
 	@Query("select avg(r.pureDegree) as avg\n"
