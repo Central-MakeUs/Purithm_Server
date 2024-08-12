6e8d8c83d85312c9fa70e21edd79f33fd182863a
tmddus2 <tmddus2__@naver.com>
2024-08-12 22:28:36 +0900

    ✨ feat: 피드 조회 API 추가

--- purithm/src/main/java/com/example/purithm/domain/feed/controller/FeedControllerDocs.java
+++ purithm/src/main/java/com/example/purithm/domain/feed/controller/FeedControllerDocs.java
@@ -1,18 +1,21 @@
 package com.example.purithm.domain.feed.controller;
 
 import com.example.purithm.domain.feed.dto.response.FeedDto;
+import com.example.purithm.domain.filter.entity.OS;
+import com.example.purithm.global.auth.annotation.LoginInfo;
 import com.example.purithm.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface FeedControllerDocs {
   @ApiResponse(responseCode = "200", description = "OK")
   public SuccessResponse<List<FeedDto>> getFeeds(
-      @RequestHeader(value = "Authorization") @Parameter(description = "인증 토큰") String authorization,
+      @LoginInfo Long id,
+      @RequestParam(value = "os", required = true) @Parameter(description = "휴대폰 os",
+          examples = {@ExampleObject(value = "AOS"), @ExampleObject(value = "iOS")}) OS os,
       @RequestParam(value = "sortedBy", required = false) @Parameter(description = "정렬순",
           examples =
               {@ExampleObject(name = "최신순", summary = "최신순 정렬", value = "latest"),
